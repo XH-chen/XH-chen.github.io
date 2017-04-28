97ff270c1a30581c718ea043347d27afc2536543
CXH <1078937456@qq.com>
2017-04-28 15:55:15 +0800

    V1.5

--- mobil/js/phoneadaptation.js
+++ mobil/js/phoneadaptation.js
@@ -3,15 +3,14 @@ var meta =document.createElement("meta");
 meta.name="viewport";
 meta.content='width=device-width,user-scalable=no,initial-scale='+i+',minimum-scale='+i+',maximum-scale='+i;
 document.getElementsByTagName("head")[0].appendChild(meta);//动态添加meta-viewport标签，设置好适配；
-
-var html = document.getElementsByTagName("html")[0];
-var iW =document.body.offsetWidth;
-var scale=iW/720*20;
-html.style.fontSize=scale+"px";//动态设置html根节点的font-size，用于rem布局；
-
+setrem(720);
 window.onresize=function(){
+	setrem(720);
+}
+function setrem(num){
 	var html = document.getElementsByTagName("html")[0];
 	var iW =document.body.offsetWidth;
-	var scale=iW/720*20;
+	var scale=(iW/num)*20;
 	html.style.fontSize=scale+"px";//动态设置html根节点的font-size，用于rem布局；
 }
+
